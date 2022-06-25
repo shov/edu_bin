937ea21e8559b1c8eb0f90cb7cd2a45013790c10
Alexander Shevchenko <ls.shov@gmail.com>
2022-06-25 10:51:46 +0200

    01 added challange 2 scripts

---
+++ 01_unity_proto/proto_2/challenge_2/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private bool lockDog = false;
+    private int lockTimer = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (!lockDog && Input.GetKeyDown(KeyCode.Space))
+        {
+            lockDog = true;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            Invoke(nameof(unlockDog), lockTimer);
+        }
+    }
+
+    void unlockDog()
+    {
+        lockDog = false;
+    }
+}
