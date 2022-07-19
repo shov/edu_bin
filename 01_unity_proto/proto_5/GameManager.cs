1df2f4115d6beee230d8aa4ce936d2a3035e0252
Alexander Shevchenko <ls.shov@gmail.com>
2022-07-19 09:39:53 +0200

    01: 5: added score, values, explosionFX

--- 01_unity_proto/proto_5/GameManager.cs
+++ 01_unity_proto/proto_5/GameManager.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targetList;
     public float spawnTimer = 2f;
 
+    public int score = 0;
+    public TextMeshProUGUI scoreText;
+
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        AddScore(0);
     }
 
     void Update()
     {
         
+    }
+
+    public void AddScore(int added)
+    {
+        score += added;
+        scoreText.text = "Score: " + score;
     }
 
     IEnumerator SpawnTarget()
--- 01_unity_proto/proto_5/Target.cs
+++ 01_unity_proto/proto_5/Target.cs
@@ -4,25 +4,53 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    public int scoreValue = 5;
+    private int randScoreRange = 5;
+
+    public ParticleSystem explosionFX;
+
     private Rigidbody rb;
+    private GameManager gameManager;
+
+    private int minForce = 12;
+    private int maxForce = 16;
+    private float xRange = 4f;
+    private float yStartBottom = -2;
+
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
-
-        // Spawn
-        transform.position = new Vector3(Random.Range(-4f, 4f), -2);
-
-        rb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
-        rb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10));
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        if(scoreValue == 0)
+        {
+            scoreValue = Random.Range(-randScoreRange, randScoreRange);
+        }
+        Spawn();
     }
 
     private void OnMouseDown()
     {
+        gameManager.AddScore(scoreValue);
+        Instantiate(explosionFX, transform.position, explosionFX.transform.rotation);
         Destroy(gameObject);
     }
 
     private void OnTriggerEnter(Collider other)
     { // must be the only one trigger there (the Sensor)
+        gameManager.AddScore(-scoreValue);
         Destroy(gameObject);
+    }
+
+    private void Spawn()
+    {
+        transform.position = new Vector3(Random.Range(-xRange, xRange), yStartBottom);
+
+        rb.AddForce(Vector3.up * Random.Range(minForce, maxForce), ForceMode.Impulse);
+        rb.AddTorque(RandomAngle(), RandomAngle(), RandomAngle());
+    }
+
+    private int RandomAngle()
+    {
+        return Random.Range(-10, 10);
     }
 }
