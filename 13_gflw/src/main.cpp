e5b3cffc7ac85d2dd9c2d3080ee6a8bea37774f7
Alexander Shevchenko <ls.shov@gmail.com>
2023-08-31 02:53:04 +0200

    13: on vertex buffer

--- 13_gflw/src/main.cpp
+++ 13_gflw/src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
 {
-    GLFWwindow* window;
+    GLFWwindow *window;
 
     /* Initialize the library */
     if (!glfwInit())
@@ -20,34 +21,37 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Error!" << std::endl;
+    }
+    else
+    {
+        std::cout << "GLEW INIT OK " << glGetString(GL_VERSION) << std::endl;
+    }
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+
+    float positions[6] = {
+        -0.5f, -0.5f, // 0
+        0.0f, 0.5f,   // 1
+        0.5f, -0.5f   // 2
+    };
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6, positions, GL_DYNAMIC_DRAW);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Draw a triangle with legacy OpenGL
-        glBegin(GL_TRIANGLES);
-        // set triangle color to green
-        glColor3f(0.0f, 1.0f, 0.0f);
-        // draw triangle
-        glVertex2f(0.0f, 0.0f);
-        glVertex2f( 0.0f,  1.0f);
-        glVertex2f( 1.0f, 0.0f);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        glVertex2f(-1.0f, 0.0f);
-        glVertex2f( 0.0f,  1.0f);
-        glVertex2f( 0.0f, 0.0f);
-
-        glVertex2f(0.0f, 0.0f);
-        glVertex2f( 0.0f, -1.0f);
-        glVertex2f( 1.0f, 0.0f);
-
-        glVertex2f(-1.0f, 0.0f);
-        glVertex2f( 0.0f, -1.0f);
-        glVertex2f( 0.0f, 0.0f);
         glEnd();
-
+        
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
